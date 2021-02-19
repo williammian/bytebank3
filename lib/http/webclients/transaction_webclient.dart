8ffcd94a6f94927a1d414577aa14a9d8308dbb14
William Mian <william_mian@yahoo.com.br>
2021-02-19 13:22:35 -0300

    conversoes json object dart

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -1,42 +1,23 @@
 import 'dart:convert';
 
 import 'package:bytebank3/http/webclient.dart';
-import 'package:bytebank3/models/contact.dart';
 import 'package:http/http.dart';
 import 'package:bytebank3/models/transaction.dart';
 
 class TransactionWebClient {
-
   Future<List<Transaction>> findAll() async {
     final Response response =
-    await client.get(baseUrl).timeout(Duration(seconds: 5));
+        await client.get(baseUrl).timeout(Duration(seconds: 5));
 
-    List<Transaction> transactions = _toTransactions(response);
-
-    return transactions;
-  }
-
-  List<Transaction> _toTransactions(Response response) {
     final List<dynamic> decodedJson = jsonDecode(response.body);
-    final List<Transaction> transactions = List();
-    for (Map<String, dynamic> transactionJson in decodedJson) {
-      final Map<String, dynamic> contactJson = transactionJson['contact'];
-      final Transaction transaction = Transaction(
-        transactionJson['value'],
-        Contact(
-          0,
-          contactJson['name'],
-          contactJson['accountNumber'],
-        ),
-      );
-      transactions.add(transaction);
-    }
-    return transactions;
+
+    return decodedJson
+        .map((dynamic json) => Transaction.fromJson(json))
+        .toList();
   }
 
   Future<Transaction> save(Transaction transaction) async {
-    Map<String, dynamic> transactionMap = _toMap(transaction);
-    final String transactionJson = jsonEncode(transactionMap);
+    final String transactionJson = jsonEncode(transaction.toJson());
 
     final Response response = await client.post(
       baseUrl,
@@ -47,31 +28,7 @@ class TransactionWebClient {
       body: transactionJson,
     );
 
-    return _toTransaction(response);
-  }
-
-  Transaction _toTransaction(Response response) {
-    Map<String, dynamic> json = jsonDecode(response.body);
-    final Map<String, dynamic> contactJson = json['contact'];
-    return Transaction(
-      contactJson['value'],
-      Contact(
-        0,
-        contactJson['name'],
-        contactJson['accountNumber'],
-      ),
-    );
-  }
-
-  Map<String, dynamic> _toMap(Transaction transaction) {
-    final Map<String, dynamic> transactionMap = {
-      'value': transaction.value,
-      'contact': {
-        'name': transaction.contact.name,
-        'accountNumber': transaction.contact.accountNumber,
-      }
-    };
-    return transactionMap;
+    return Transaction.fromJson(jsonDecode(response.body));
   }
 
 }
--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -9,8 +9,21 @@ class Contact {
     this.accountNumber,
   );
 
+  Contact.fromJson(Map<String, dynamic> json) :
+        id = json['id'],
+        name = json['name'],
+        accountNumber = json['accountNumber'];
+
+  Map<String, dynamic> toJson() =>
+      {
+        'id' : id,
+        'name': name,
+        'accountNumber': accountNumber,
+      };
+
   @override
   String toString() {
     return 'Contact{id: $id, name: $name, accountNumber: $accountNumber}';
   }
+
 }
